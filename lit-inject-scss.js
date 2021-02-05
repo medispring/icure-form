40cc8b1e627febbd2a8380a13bf625a6ce5b34d8
Antoine Duchâteau <ad@taktik.com>
2021-02-05 11:10:48 +0100

    inject css before compiling with tsc

---
+++ lit-inject-scss.js
@@ -0,0 +1,40 @@
+const { join, relative, dirname } = require('path');
+const { readdir, mkdir, copyFile, readFile, writeFile } = require('fs').promises;
+const sass = require('sass')
+
+const args = process.argv.slice(2);
+
+async function* getFiles(dir) {
+	const entries = await readdir(dir, { withFileTypes: true })
+	for (const entry of entries) {
+		const res = join(dir, entry.name)
+		if (entry.isDirectory()) {
+			yield* getFiles(res)
+		} else {
+			yield res
+		}
+	}
+}
+
+;(async () => {
+	const src = args[0];
+	const dst = args[1];
+	for await (const f of getFiles(src)) {
+		const df = join(dst, relative(src, f))
+		await mkdir(dirname(df), {recursive:true})
+		if (f.match(/.+(\.tsx?|\.js)/)) {
+			const txt = await readFile(f, 'UTF-8')
+			let isFirst = true
+			const treated = txt.replaceAll(/import\s+(.+)\s+from\s('(.+?\.s?css)'|"(.+?\.s?css)").*/g, (_, varName, g, sQuotedPath, dQuotedPath) => {
+				const scss = join(dirname(f), sQuotedPath || dQuotedPath)
+				const { css } =  sass.renderSync({file: scss});
+				const replacement = `${isFirst ? "import { css } from 'lit-element';\n" : ''}const ${varName} = css\`${css.toString()}\` `;
+				isFirst = false
+				return replacement
+			})
+			await writeFile(df, treated)
+		} else if (f.match(/.+(\.s?css)/)) {
+			await copyFile(f, df)
+		}
+	}
+})()
