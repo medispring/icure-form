b64eeccd0a4623f2c17efb1232dc30598cc323a3
Friedrich Dylan <dylan.friedrich@medispring.be>
2023-03-10 16:22:23 +0100

    proper generateLabels

--- src/components/iqr-label/utils.ts
+++ src/components/iqr-label/utils.ts
@@ -2,15 +2,7 @@ import { html, TemplateResult } from 'lit'
 import { Labels } from '../iqr-text-field'
 
 export function generateLabels(labels: Labels): TemplateResult[] {
-	return Object.keys(labels).map((position) => {
-		switch (position) {
-			case 'right':
-			case 'left':
-				return html` <label class="iqr-label side above ${position}"><span>${labels[position]}</span></iqr-label> `
-			default:
-				return html` <label class="iqr-label ${position}"><span>${labels[position]}</span></iqr-label> `
-		}
-	})
+	return Object.keys(labels).map((position) => generateLabel(position, labels[position]))
 }
 
 export function generateLabel(label: string, labelPosition: string): TemplateResult {
