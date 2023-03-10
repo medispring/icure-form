8640e641350fad6304d7ee3662a96e719a3a4587
Friedrich Dylan <dylan.friedrich@medispring.be>
2023-03-10 17:39:08 +0100

    fix labeks

--- src/components/iqr-label/utils.ts
+++ src/components/iqr-label/utils.ts
@@ -2,7 +2,7 @@ import { html, TemplateResult } from 'lit'
 import { Labels } from '../iqr-text-field'
 
 export function generateLabels(labels: Labels): TemplateResult[] {
-	return Object.keys(labels).map((position) => generateLabel(position, labels[position]))
+	return Object.keys(labels).map((position) => generateLabel(labels[position], position))
 }
 
 export function generateLabel(label: string, labelPosition: string): TemplateResult {
