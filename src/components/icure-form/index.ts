2c518bb23d4126aa1f6f1bc905bc6eb6a34aadeb
aduchate <ad@icure.com>
2024-01-15 18:08:22 +0100

    Preliminary work to support default values in iCure forms.

--- src/components/icure-form/index.ts
+++ src/components/icure-form/index.ts
@@ -4,12 +4,14 @@ import { property } from 'lit/decorators.js'
 
 import { Renderer } from './renderer'
 import { render as renderAsForm } from './renderer/form/form'
-import { Code, FieldMetadata, FieldValue, Form } from '../model'
+import { Code, Field, FieldMetadata, FieldValue, Form, Group, Subform } from '../model'
 import { FormValuesContainer } from '../../generic'
 
 // @ts-ignore
 import baseCss from '../common/styles/style.scss'
 import { defaultTranslationProvider } from '../../utils/languages'
+import { PropertyValues } from '@lit/reactive-element'
+import { fieldValuesProvider } from '../../utils/fields-values-provider'
 
 /**
  * Form element
@@ -34,6 +36,38 @@ export class IcureForm extends LitElement {
 
 	static get styles() {
 		return [baseCss]
+	}
+
+	extractDefaultValues(fgs: Array<Field | Group | Subform>) {
+		const fvc = this.formValuesContainer
+		if (fvc) {
+			fgs.forEach((fg) => {
+				if (fg.clazz === 'group' && fg.fields?.length) {
+					this.extractDefaultValues(fg.fields)
+				} else if (fg.clazz === 'field') {
+					//Check that no value already exists
+					if (!Object.keys(fieldValuesProvider(fvc, fg)() ?? {}).length) {
+						if (fg.computedProperties?.defaultValue) {
+							const value: FieldValue | undefined = fvc?.compute(fg.computedProperties?.defaultValue)
+							if (value) {
+								//TODO support batch changes
+								fvc?.setValue((fg as Field).label(), this.displayedLanguage ?? 'en', value)
+							}
+						}
+					}
+				}
+			})
+		}
+	}
+
+	protected firstUpdated(_changedProperties: PropertyValues) {
+		super.firstUpdated(_changedProperties)
+
+		if (this.formValuesContainer) {
+			this.form?.sections.forEach((section) => {
+				this.extractDefaultValues(section.fields ?? [])
+			})
+		}
 	}
 
 	render() {
