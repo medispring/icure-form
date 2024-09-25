ef7e4107f49d8c372e6805f9fabc14c9376f3a4b
Maxime Mennechet <maxime.mennechet@gmail.com>
2024-09-25 16:01:06 +0200

    Wip on okido form

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -42,8 +42,6 @@ import okido_anamnesis from './samples/okido_anamnesis.yaml'
 // @ts-ignore
 import okido_search from './samples/okido_search.yaml'
 // @ts-ignore
-import okido_measure from './samples/okido_measure.yaml'
-// @ts-ignore
 import okido_evaluation from './samples/okido_evaluation.yaml'
 // @ts-ignore
 import okido_planification from './samples/okido_planification.yaml'
@@ -86,9 +84,9 @@ class DemoApp extends LitElement {
 		...[
 			/*{ title: '000 - Validators', form: Form.parse(YAML.parse(validators)) },
 			{ title: '01 - Token fields', form: Form.parse(YAML.parse(tokenFields)) },
-			{ title: '1 - BMI', form: Form.parse(YAML.parse(bmi)) },*/
-			/*{ title: '1 - Time of appointment', form: Form.parse(YAML.parse(time_of_appointment)) },*/
-			/*{ title: 'Obstetrics', form: convertLegacy(obstetrics as FormLayout, legacyForms) },
+			{ title: '1 - BMI', form: Form.parse(YAML.parse(bmi)) },
+			{ title: '1 - Time of appointment', form: Form.parse(YAML.parse(time_of_appointment)) },
+			{ title: 'Obstetrics', form: convertLegacy(obstetrics as FormLayout, legacyForms) },
 			{ title: '2 - Preliminary psycho-social interview', form: Form.parse(YAML.parse(preliminary_psycho_social_interview)) },
 			{ title: '3 - Preliminary medical interview', form: Form.parse(YAML.parse(preliminary_medical_interview)) },
 			{ title: '4 - Termination of pregnancy curetage', form: Form.parse(YAML.parse(termination_of_pregnancy_curetage)) },
@@ -98,14 +96,13 @@ class DemoApp extends LitElement {
 			{ title: '8 - Extra', form: Form.parse(YAML.parse(extra)) },
 			{ title: '9 - Note', form: Form.parse(YAML.parse(note)) },*/
 			{ title: 'OKIDO - Anamnèse', form: Form.parse(YAML.parse(okido_anamnesis)) },
-			{ title: 'OKIDO - Recherche', form: Form.parse(YAML.parse(okido_measure)) },
 			{ title: 'OKIDO - Recherche', form: Form.parse(YAML.parse(okido_search)) },
-			{ title: 'OKIDO - Evaluation', form: Form.parse(YAML.parse(okido_evaluation)) },
+			/*{ title: 'OKIDO - Evaluation', form: Form.parse(YAML.parse(okido_evaluation)) },
 			{ title: 'OKIDO - Planification', form: Form.parse(YAML.parse(okido_planification)) },
 			{ title: 'OKIDO - Traitement', form: Form.parse(YAML.parse(okido_treatment)) },
 			{ title: 'OKIDO - Diagnostique', form: Form.parse(YAML.parse(okido_diagnostic)) },
 			{ title: 'OKIDO - Clôture', form: Form.parse(YAML.parse(okido_closure)) },
-			/*{ title: 'OKIDO - Anamnèse - Trouble fonctionnel', form: Form.parse(YAML.parse(okido_functional_disorder)) },
+			{ title: 'OKIDO - Anamnèse - Trouble fonctionnel', form: Form.parse(YAML.parse(okido_functional_disorder)) },
 			{ title: 'OKIDO - Anamnèse - Répercution / Sévérité', form: Form.parse(YAML.parse(okido_severity_disorder)) },
 			{ title: 'OKIDO - Anamnèse - Facteur externe', form: Form.parse(YAML.parse(okido_external_factor)) },
 			{ title: 'OKIDO - Anamnèse - Facteur individuel / personnel', form: Form.parse(YAML.parse(okido_individual_factor)) },
@@ -169,6 +166,9 @@ class DemoApp extends LitElement {
 
 	connectedCallback() {
 		super.connectedCallback()
+
+		console.log('obs', this.samples[4].form.toJson())
+
 		window.onkeydown = (event) => {
 			if ((event.key === 'Z' || event.key === 'z') && event.metaKey) {
 				console.log(event.key)
