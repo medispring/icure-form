623bf590ff82cfaeee0e651a12563ebe7c48df46
aduchate <ad@icure.com>
2024-09-26 13:06:01 +0200

    Translations for labels

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -82,11 +82,11 @@ class DemoApp extends LitElement {
 	private hcpApi: IccHcpartyXApi = new IccHcpartyXApi('https://kraken.svc.icure.cloud/rest/v1', { Authorization: 'Basic YWJkZW1vQGljdXJlLmNsb3VkOmtuYWxvdQ==' })
 	private samples = [
 		...[
-			/*{ title: '000 - Validators', form: Form.parse(YAML.parse(validators)) },
+			{ title: '000 - Validators', form: Form.parse(YAML.parse(validators)) },
 			{ title: '01 - Token fields', form: Form.parse(YAML.parse(tokenFields)) },
 			{ title: '1 - BMI', form: Form.parse(YAML.parse(bmi)) },
 			/*{ title: '1 - Time of appointment', form: Form.parse(YAML.parse(time_of_appointment)) },*/
-			/*{ title: 'Obstetrics', form: convertLegacy(obstetrics as FormLayout, legacyForms) },
+			{ title: 'Obstetrics', form: convertLegacy(obstetrics as FormLayout, legacyForms) },
 			{ title: '2 - Preliminary psycho-social interview', form: Form.parse(YAML.parse(preliminary_psycho_social_interview)) },
 			{ title: '3 - Preliminary medical interview', form: Form.parse(YAML.parse(preliminary_medical_interview)) },
 			{ title: '4 - Termination of pregnancy curetage', form: Form.parse(YAML.parse(termination_of_pregnancy_curetage)) },
--- src/utils/form-value-container.ts
+++ src/utils/form-value-container.ts
@@ -1,49 +1,37 @@
 import { Field, FieldMetadata, FieldValue, Form, Group, Subform } from '../components/model'
-import { fieldValuesProvider, makeMetadata } from './fields-values-provider'
+import { makeMetadata } from './fields-values-provider'
 import { FormValuesContainer } from '../generic'
 
-export const initializeWithFormDefaultValues = (
+export const computeFormDefaultValues = (
 	formValuesContainer: FormValuesContainer<FieldValue, FieldMetadata>,
-	form: Form,
+	form?: Form,
 	language?: string,
 	owner?: string,
-): FormValuesContainer<FieldValue, FieldMetadata> => {
-	const fvc = [formValuesContainer]
-
-	const extractDefaultValues = (fgs: Array<Field | Group | Subform>, language?: string) => {
-		if (fvc[0]) {
-			fgs.forEach((fg) => {
-				if (fg.clazz === 'group' && fg.fields?.length) {
-					extractDefaultValues(fg.fields, language)
-				} else if (fg.clazz === 'field') {
-					//Check that no value already exists
-					if (!Object.keys(fieldValuesProvider(fvc[0], fg)() ?? {}).length) {
-						if (fg.computedProperties?.defaultValue) {
-							const value: FieldValue | undefined = fvc[0]?.compute(fg.computedProperties?.defaultValue)
-							if (value !== undefined) {
-								const lng = language ?? 'en'
-								if (!value.content[lng] && value.content['*']) {
-									value.content[lng] = value.content['*']
-								}
-								//TODO support batch changes
-								fvc[0]?.setValue((fg as Field).label(), lng, value, undefined, makeMetadata(fg, owner))
-							}
+): [FieldValue, FieldMetadata][] => {
+	const extractDefaultValues = (fgs: Array<Field | Group | Subform>, language?: string): [FieldValue, FieldMetadata][] =>
+		fgs.reduce((acc, fg) => {
+			if (fg.clazz === 'group' && fg.fields?.length) {
+				return [...acc, ...extractDefaultValues(fg.fields, language)]
+			} else if (fg.clazz === 'field') {
+				if (fg.computedProperties?.defaultValue) {
+					const value: FieldValue | undefined = formValuesContainer?.compute(fg.computedProperties?.defaultValue)
+					if (value !== undefined) {
+						const lng = language ?? 'en'
+						if (!value.content[lng] && value.content['*']) {
+							value.content[lng] = value.content['*']
+						}
+						if (value.content[lng]) {
+							return [...acc, [value, makeMetadata(fg, owner)]]
 						}
 					}
 				}
-			})
-		}
-	}
+			}
+			return acc
+		}, [] as [FieldValue, FieldMetadata][])
 
-	const listener = (newValue: FormValuesContainer<FieldValue, FieldMetadata>) => {
-		fvc[0] = newValue
-	}
-	fvc[0].registerChangeListener(listener)
-
-	form?.sections.forEach((section) => {
-		extractDefaultValues(section.fields ?? [], language)
-	})
-
-	fvc[0].unregisterChangeListener(listener)
-	return fvc[0]
+	return (
+		form?.sections.reduce((acc, section) => {
+			return [...acc, ...extractDefaultValues(section.fields ?? [], language)]
+		}, [] as [FieldValue, FieldMetadata][]) ?? []
+	)
 }
